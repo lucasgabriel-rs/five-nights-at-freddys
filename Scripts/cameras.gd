221c85ce699488963d0595771d3fdc3b48116374
NightSK5 <lucasgabrielramosdasilva94@gmail.com>
2025-04-29 15:42:27 -0300

    bonnie ai 90% complete

--- Scripts/cameras.gd
+++ Scripts/cameras.gd
@@ -1,147 +1,193 @@
 extends Node2D
 
+var cameras: Array = [
+	$Stage, # id 0
+	$"Dining Area", # id 1
+	$"Pirate Cove", # id 2
+	$Restrooms, # id 3
+	$Backstage, # id 4
+	$"West Hall Corner", # id 5
+	$"East Hall Corner", # id 6
+	$"West Hall", # id 7
+	$"East Hall", # id 8
+	$Kitchen # id 9
+]
 @onready var supply_room: AnimatedSprite2D = $"UI/Supply Room"
 
 
 func _on_cam_1a_pressed() -> void:
-	$Stage.visible = true
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = true
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_1b_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = true
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = true
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_1c_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = true
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = true
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_2a_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = true
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = true
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_2b_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = true
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = true
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_3_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = true
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_4a_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = true
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = true
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_4b_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = true
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = true
+	cameras[9].visible = false
 
 func _on_cam_5_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = true
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = true
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
 
 func _on_cam_6_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = false
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = false
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = true
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = true
 
 func _on_cam_7_pressed() -> void:
-	$Stage.visible = false
-	$"Dining Area".visible = false
-	$"Pirate Cove".visible = false
-	$Restrooms.visible = true
-	$Backstage.visible = false
-	$"West Hall Corner".visible = false
-	$"East Hall Corner".visible = false
+	cameras[0].visible = false
+	cameras[1].visible = false
+	cameras[2].visible = false
+	cameras[3].visible = true
+	cameras[4].visible = false
+	cameras[5].visible = false
+	cameras[6].visible = false
 	supply_room.visible = false
-	$"West Hall".visible = false
-	$"East Hall".visible = false
-	$Kitchen.visible = false
+	cameras[7].visible = false
+	cameras[8].visible = false
+	cameras[9].visible = false
+
+func _on_timer_timeout() -> void:
+	Global.ai("bonnie")
+	cameraUpdate("bonnie")
+
+func cameraUpdate(animatronic: String) -> void:
+	match animatronic:
+		"bonnie":
+			match Global.animatronicPos(animatronic):
+				"dining area":
+					cameras[0].play("chica and freddy")
+					cameras[1].play(["bonnie 1", "bonnie 2"].pick_random())
+					cameras[4].play("default")
+					cameras[5].play("default")
+				"backstage":
+					cameras[0].play("chica and freddy")
+					cameras[1].play("default")
+					cameras[4].play(["bonnie 1", "bonnie 2"].pick_random())
+					cameras[5].play("default")
+				"west hall corner":
+					cameras[1].play("default")
+					cameras[5].play("bonnie")
+					supply_room.play("default")
+					cameras[7].play("default")
+				"supply room":
+					cameras[5].play("default")
+					supply_room.play("bonnie")
+					cameras[7].play("default")
+				"west hall":
+					cameras[5].play("default")
+					supply_room.play("default")
+					cameras[7].play("bonnie")
+				"door":
+					pass
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,4 +1,33 @@
 extends Node
 
-var leftDoor: bool = false
-var rightDoor: bool = false
+# Night Counter
+var night: int
+
+# Doors
+var leftDoor: bool
+var rightDoor: bool
+
+# Animatronics
+var animatronics: Dictionary = {
+	"bonnie": {
+		"difficult": [0, 0, 0, 0, 0],
+		"pos": "stage"
+	},
+}
+
+func animatronicPos(x: String):
+	return animatronics[x]["pos"]
+
+func ai(n: String) -> void:
+	var difficult = animatronics[n]["difficult"][night]
+	var dice = randi_range(1, 20)
+	if difficult >= dice:
+		match animatronics[n]["pos"]:
+			"dining area": animatronics[n]["pos"] = ["west hall corner", "backstage"].pick_random()
+			"backstage": animatronics[n]["pos"] = ["dining area", "west hall corner"].pick_random()
+			"west hall corner": animatronics[n]["pos"] = ["dining area", "supply room", "west hall"].pick_random()
+			"supply room": animatronics[n]["pos"] = ["west hall corner", "west hall"].pick_random()
+			"west hall": animatronics[n]["pos"] = ["west hall corner", "supply room"].pick_random()
+			"door": pass
+			_: animatronics[n]["pos"] = ["dining area", "backstage"].pick_random()
+	#print("Dice: ", dice,"\nPosition: ", animatronicPos(n))
