66fcd36d816d28899c1a5cb122e8123032f27bac
HYZIN-1 <araujo.rplucas@gmail.com>
2025-05-02 16:40:22 -0300

    Game Over Screen & Buttons

---
+++ Scripts/gameover.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _on_retry_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Office/office.tscn")
+
+func _on_retry_button_mouse_entered() -> void:
+	$Arrow.position.x = $"retry_button".position.x
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+func _on_main_menu_button_mouse_entered() -> void:
+	$Arrow.position.x = $"main_menu_button".position.x
